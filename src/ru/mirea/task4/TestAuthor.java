0ef61561da7361b039255823b5c0109d0151ee68
TrofimovMA <svintyc@mail.ru>
2021-10-01 11:45:00 +0300

    2021.10.01 11:45

--- src/ru/mirea/task4/TestAuthor.java
+++ src/ru/mirea/task4/TestAuthor.java
@@ -5,6 +5,7 @@ public class TestAuthor {
     {
         Author a1 = new Author("Author1", "a@a.ru", 'M');
         System.out.println(a1);
+        System.out.println("Changing Author1's mail to b@b.ru.");
         a1.setEmail("b@b.ru");
         System.out.println(a1);
     }
